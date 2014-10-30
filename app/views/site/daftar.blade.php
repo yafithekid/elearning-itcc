45bdeefd2de0bdbf1f724efa7f1002d759a0714a
opelhoward <opelhoward@yahoo.com>
2014-10-30 14:31:43 +0700

    add daftar and login

--- app/views/site/daftar.blade.php
+++ app/views/site/daftar.blade.php
@@ -4,16 +4,31 @@
 <h1>Daftar</h1>
 <form action='daftar' method='post'>
 	<?= Form::token();?>
-	
+	<?php var_dump($errors->all()); ?>
 	<div>
 		<label for="username">Username</label>
 		<input type="text" name="username" value='<?=Input::old('username')?>'>
 		<span>{{$errors->first('username')}}</span>
 	</div>
 	<div>
-		<label for="username">Password</label>
+		<label for="password">Password</label>
 		<input type="password" name="password">
 		<span>{{$errors->first('password')}}</span>
+	</div>
+	<div>
+		<label for="email">Email</label>
+		<input type="email" name="email" value='<?=Input::old('email')?>'>
+		<span>{{$errors->first('email')}}</span>
+	</div>
+	<div>
+		<label for="fullname">Fullname</label>
+		<input type="text" name="fullname" value='<?=Input::old('fullname')?>'>
+		<span>{{$errors->first('fullname')}}</span>
+	</div>
+	<div>
+		<label for="school">School</label>
+		<input type="text" name="school" value='<?=Input::old('school')?>'>
+		<span>{{$errors->first('school')}}</span>
 	</div>
 	<button type="submit">Daftar</button>
 </form>
